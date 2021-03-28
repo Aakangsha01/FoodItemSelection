030a8a7c89018ac024ed9987e45acc4ccfae3d23
Aakangsha01 <81461564+Aakangsha01@users.noreply.github.com>
2021-03-28 12:05:18 +0530

    Add files via upload

---
+++ FoodItemSelection.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+main()
+{
+	 int n;
+	 printf("Enter any number from 1 to 5:  ");
+	 scanf("%d",&n);
+	 switch(n)
+	 {
+	 	case 1: 
+	 	  printf("\nFood Item- Pasta \nPrice- Rs 179");
+	 	  break;
+	 	case2:
+	 	  printf("\nFood Item- Pizza \nPrice- Rs 239"); 
+	 	  break;
+	 	case 3:
+	 	  printf("\nFood Item- Burger \nPrice- Rs 129");
+	 	  break;
+	 	case 4:
+	 	  printf("\nFood Item- French Fries \nPrice- Rs 99");
+	 	  break;
+	 	case 5:
+	 	  printf("\nFood Item- Sandwich \nPrice- Rs 149");
+	 	  break;
+}
+}
+	 	  
